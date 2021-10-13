ed4c8e2bf094eee200e1e0cfb3987f469e5ae3b0
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-10-13 18:21:52 -0300

    requisito2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqu
-}
+// eslint-disable-next-line no-unused-vars
+const { species } = data;
+
+const getAnimalsOlderThan = (animal, age) => {
+  const animalAtual = species.find((specie) => specie.name === animal);
+  console.log(animalAtual);
+  const result = animalAtual.residents.every((resident) => resident.age >= age);
+  return result;
+};
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('penguins', 10));
