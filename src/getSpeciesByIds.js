2b5f6e56278e0cd14d096c221ce669453588020a
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-10-13 17:06:19 -0300

    requisito1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
 
+  const result = ids.map((id) => species.find((specie) => specie.id === id));
+  return result;
+}
 module.exports = getSpeciesByIds;
+// getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
