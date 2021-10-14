baddcaa4b78efad9b055976687735d402b5cd5f0
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-10-14 20:05:02 -0300

    requisito5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    const lista = {};
+    species.forEach((specie) => {
+      const nome = specie.name;
+      const quantidade = specie.residents.length;
+      lista[nome] = quantidade;
+    });
+    return lista;
+  } if (animal.sex === undefined) {
+    const quantAnimais = species.find((specie1) => specie1.name === animal.specie);
+    return quantAnimais.residents.length;
+  }
+  const quantAnimais = species.find((specie1) => specie1.name === animal.specie);
+  const quantAnimaisFiltrados = quantAnimais.residents
+    .filter((filterAnimal) => filterAnimal.sex === animal.sex);
+  return quantAnimaisFiltrados.length;
+}
 module.exports = countAnimals;
+
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
