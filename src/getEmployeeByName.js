b45c6ce0fd5aa46a099e4835df0d35c238892e69
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-10-14 15:46:02 -0300

    requisito3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName !== 'string') {
+    return {};
+  }
+  const result = employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return result;
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName());
